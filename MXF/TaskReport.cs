dd32da65ecc7468ffdff2d639ba5611b4b85af78
feigenanton <feigeanton@hotmail.de>
2025-03-09 23:18:10 +0100

    change class to record, add arguments check

--- MXF/TaskReport.cs
+++ MXF/TaskReport.cs
@@ -1,12 +1,18 @@
-﻿namespace Myriadbits.MXF
+﻿using System;
+
+namespace Myriadbits.MXF
 {
-    public class TaskReport
+    public record TaskReport
     {
-        public string Description { get; set; }
-        public int Percent { get; set; }
+        public string Description { get; init; }
+        public int Percent { get; init; }
 
         public TaskReport(int percent, string desc)
         {
+            if(percent > 100 || percent < 0)
+            {
+                throw new ArgumentOutOfRangeException("Percent must be between 0 and 100");
+            }
             Description = desc;
             Percent = percent;
         }
